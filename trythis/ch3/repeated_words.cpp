36f3c8d7700e2fe5f5d8fa49f6d975403d182276
Tristan Burgess <tburgessdev@gmail.com>
2023-02-08 19:43:50 -0500

    ch3 trythis part 4 adding counter

--- trythis/ch3/repeated_words.cpp
+++ trythis/ch3/repeated_words.cpp
@@ -1,11 +1,13 @@
 #include "std_lib_facilities.h"
 
 int main() {
+    int wordCount = 0;
     string prev = " ";
     string cur;
     while (cin >> cur) {
+        ++wordCount;
         if (prev == cur) {
-            cout << "Repeated word: " << cur << '\n';
+            cout << "word number: " << wordCount << "\trepeated word: " << cur << '\n';
         }
         prev = cur;
     }
